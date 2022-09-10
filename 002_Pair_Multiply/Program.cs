43be639c5188b3387d10e25c62ac8c5aa2bcee08
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-10 11:31:14 +0300

    Added second file

---
+++ 002_Pair_Multiply/Program.cs
@@ -0,0 +1,22 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(1, 11);
+}
+void PrintArray( int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write(array[i] + " ");
+}
+void MultiplyFinder(int[] array)
+{
+    for (int i = 0; i < array.Length/2; i += 1)
+    {
+        Console.WriteLine($"\n {array[i]} * {array[array.Length - 1 - i]} = {array[i] * array[array.Length - 1 - i]}");
+    }
+}
+Console.WriteLine("Введите длину массива");
+int length = int.Parse(Console.ReadLine() ?? "0");
+
+int[] massiv = new int[length];
+FillArray(massiv);
+PrintArray(massiv);
+MultiplyFinder(massiv);
