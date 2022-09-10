0b5ba103b1e5bda42d51bf9d06d2e6770d23cadf
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-10 15:56:38 +0300

    Added final save

---
+++ 003_Difference/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i += 1)
+    {
+        Console.Write($"Введите {i+1} элемент:");
+        array[i] = double.Parse(Console.ReadLine() ?? "0");
+    }
+}
+void DifferFind(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i += 1)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+    }
+    Console.WriteLine(max-min);
+}
+
+
+Console.WriteLine("Введите длинну массива");
+int length = int.Parse(Console.ReadLine() ?? "0");
+double[] array = new double[length];
+FillArray(array);
+DifferFind(array);
